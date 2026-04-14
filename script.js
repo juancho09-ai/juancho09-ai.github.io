eac5acac6297d14d896ebc6300538248a3306cba
Juancho <juandddg89@gmail.com>
2026-04-14 13:45:22 -0500

    nuevos arreglos tienda

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
 const juegos = [
-    {nombre:"God of War (2018)", precio:50, plataforma:"ps"},
-    {nombre:"God of War Ragnarök", precio:60, plataforma:"ps"},
-    {nombre:"The Last of Us Part I", precio:50, plataforma:"ps"},
-    {nombre:"The Last of Us Part II", precio:50, plataforma:"ps"},
-    {nombre:"Bloodborne", precio:40, plataforma:"ps"},
-    {nombre:"Ghost of Tsushima", precio:55, plataforma:"ps"},
-    {nombre:"Marvel's Spider-Man", precio:50, plataforma:"ps"},
-    {nombre:"Spider-Man 2", precio:60, plataforma:"ps"},
-    {nombre:"Horizon Zero Dawn", precio:40, plataforma:"ps"},
-    {nombre:"Horizon Forbidden West", precio:60, plataforma:"ps"},
-    {nombre:"Helldivers 2", precio:50, plataforma:"ps"},
+    // PLAYSTATION
+    {nombre:"God of War (2018)", precio:50, plataforma:"ps", imagen:"img/gow2018.jpg"},
+    {nombre:"God of War Ragnarök", precio:60, plataforma:"ps", imagen:"img/gowragnarok.jpg"},
+    {nombre:"The Last of Us Part I", precio:50, plataforma:"ps", imagen:"img/tlou1.jpg"},
+    {nombre:"The Last of Us Part II", precio:50, plataforma:"ps", imagen:"img/tlou2.jpg"},
+    {nombre:"Bloodborne", precio:40, plataforma:"ps", imagen:"img/bloodborne.jpg"},
+    {nombre:"Ghost of Tsushima", precio:55, plataforma:"ps", imagen:"img/tsushima.jpg"},
+    {nombre:"Marvel's Spider-Man", precio:50, plataforma:"ps", imagen:"img/spiderman.jpg"},
+    {nombre:"Spider-Man 2", precio:60, plataforma:"ps", imagen:"img/spiderman2.jpg"},
+    {nombre:"Horizon Zero Dawn", precio:40, plataforma:"ps", imagen:"img/hzd.jpg"},
+    {nombre:"Horizon Forbidden West", precio:60, plataforma:"ps", imagen:"img/hfw.jpg"},
+    {nombre:"Helldivers 2", precio:50, plataforma:"ps", imagen:"img/helldivers2.jpg"},
+    {nombre:"Final Fantasy VII Rebirth", precio:70, plataforma:"ps", imagen:"img/ff7.jpg"},
 
-    {nombre:"Red Dead Redemption 2", precio:60, plataforma:"xbox"},
-    {nombre:"Elden Ring", precio:60, plataforma:"xbox"},
-    {nombre:"The Witcher 3", precio:40, plataforma:"xbox"},
-    {nombre:"Cyberpunk 2077", precio:50, plataforma:"xbox"},
-    {nombre:"Doom Eternal", precio:40, plataforma:"xbox"},
-    {nombre:"Resident Evil 4 Remake", precio:60, plataforma:"xbox"},
-    {nombre:"Alan Wake 2", precio:60, plataforma:"xbox"},
+    // XBOX
+    {nombre:"Red Dead Redemption 2", precio:60, plataforma:"xbox", imagen:"img/rdr2.jpg"},
+    {nombre:"Elden Ring", precio:60, plataforma:"xbox", imagen:"img/eldenring.jpg"},
+    {nombre:"The Witcher 3: Wild Hunt", precio:40, plataforma:"xbox", imagen:"img/witcher3.jpg"},
+    {nombre:"Cyberpunk 2077", precio:50, plataforma:"xbox", imagen:"img/cyberpunk.jpg"},
+    {nombre:"Doom Eternal", precio:40, plataforma:"xbox", imagen:"img/doom.jpg"},
+    {nombre:"Resident Evil 4 Remake", precio:60, plataforma:"xbox", imagen:"img/re4.jpg"},
+    {nombre:"Alan Wake 2", precio:60, plataforma:"xbox", imagen:"img/alanwake2.jpg"},
 
-    {nombre:"Baldur's Gate 3", precio:60, plataforma:"pc"},
-    {nombre:"Disco Elysium", precio:40, plataforma:"pc"},
-    {nombre:"Hades", precio:30, plataforma:"pc"},
-    {nombre:"Stardew Valley", precio:20, plataforma:"pc"},
-    {nombre:"It Takes Two", precio:35, plataforma:"pc"},
-    {nombre:"Mass Effect Legendary Edition", precio:50, plataforma:"pc"},
-    {nombre:"Sekiro: Shadows Die Twice", precio:60, plataforma:"pc"},
-    {nombre:"Minecraft", precio:30, plataforma:"pc"},
+    // PC
+    {nombre:"Baldur's Gate 3", precio:60, plataforma:"pc", imagen:"img/bg3.jpg"},
+    {nombre:"Disco Elysium: The Final Cut", precio:40, plataforma:"pc", imagen:"img/disco.jpg"},
+    {nombre:"Hades", precio:30, plataforma:"pc", imagen:"img/hades.jpg"},
+    {nombre:"Stardew Valley", precio:20, plataforma:"pc", imagen:"img/stardew.jpg"},
+    {nombre:"It Takes Two", precio:35, plataforma:"pc", imagen:"img/ittakestwo.jpg"},
+    {nombre:"Mass Effect Legendary Edition", precio:50, plataforma:"pc", imagen:"img/masseffect.jpg"},
+    {nombre:"Sekiro: Shadows Die Twice", precio:60, plataforma:"pc", imagen:"img/sekiro.jpg"},
+    {nombre:"Minecraft", precio:30, plataforma:"pc", imagen:"img/minecraft.jpg"},
+    {nombre:"Grand Theft Auto V", precio:40, plataforma:"pc", imagen:"img/gta5.jpg"},
+    {nombre:"Persona 5 Royal", precio:50, plataforma:"pc", imagen:"img/persona5.jpg"},
+    {nombre:"Hollow Knight", precio:25, plataforma:"pc", imagen:"img/hollowknight.jpg"},
 
-    {nombre:"Zelda: Breath of the Wild", precio:60, plataforma:"nintendo"},
-    {nombre:"Zelda: Tears of the Kingdom", precio:70, plataforma:"nintendo"},
-    {nombre:"Super Mario Odyssey", precio:55, plataforma:"nintendo"},
-    {nombre:"Mario Kart 8 Deluxe", precio:50, plataforma:"nintendo"},
-    {nombre:"Super Smash Bros Ultimate", precio:60, plataforma:"nintendo"},
-
-    {nombre:"Grand Theft Auto V", precio:40, plataforma:"pc"},
-    {nombre:"Persona 5 Royal", precio:50, plataforma:"pc"},
-    {nombre:"Final Fantasy VII Rebirth", precio:70, plataforma:"ps"}
+    // NINTENDO
+    {nombre:"Zelda: Breath of the Wild", precio:60, plataforma:"nintendo", imagen:"img/botw.jpg"},
+    {nombre:"Zelda: Tears of the Kingdom", precio:70, plataforma:"nintendo", imagen:"img/totk.jpg"},
+    {nombre:"Super Mario Odyssey", precio:55, plataforma:"nintendo", imagen:"img/mario.jpg"},
+    {nombre:"Mario Kart 8 Deluxe", precio:50, plataforma:"nintendo", imagen:"img/mariokart.jpg"},
+    {nombre:"Super Smash Bros Ultimate", precio:60, plataforma:"nintendo", imagen:"img/smash.jpg"}
 ];
 
 let carrito = [];
@@ -54,7 +58,7 @@ function mostrarJuegos(filtro) {
         div.classList.add("card");
 
         div.innerHTML = `
-            <img src="https://via.placeholder.com/300x200" alt="${juego.nombre}">
+            <img src="${juego.imagen}" alt="${juego.nombre}">
             <h3>${juego.nombre}</h3>
             <p>$${juego.precio}</p>
             <button onclick="agregar('${juego.nombre}', ${juego.precio})">Agregar</button>
@@ -109,14 +113,67 @@ function actualizarCarrito() {
 }
 
 function toggleCarrito() {
-    const carrito = document.getElementById("carrito");
-    carrito.classList.toggle("oculto");
+    document.getElementById("carrito").classList.toggle("oculto");
 }
 
 function eliminar(index) {
     carrito.splice(index, 1);
     actualizarCarrito();
 }
+
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("oculto");
+}
+
+// MODAL
+function generarFactura() {
+    if (carrito.length === 0) {
+        alert("Tu carrito está vacío 😢");
+        return;
+    }
+
+    document.getElementById("modal").classList.add("activo");
+}
+
+function cerrarModal() {
+    document.getElementById("modal").classList.remove("activo");
+}
+
+function confirmarCompra() {
+    localStorage.setItem("misJuegos", JSON.stringify(carrito));
+
+    alert("✅ Tu compra se ha realizado con éxito");
+
+    carrito.length = 0;
+    actualizarCarrito();
+
+    cerrarModal();
+    mostrarMisJuegos();
+}
+
+// MIS JUEGOS
+function mostrarMisJuegos() {
+    document.getElementById("inicio").style.display = "none";
+    lista.innerHTML = "";
+
+    let juegosComprados = JSON.parse(localStorage.getItem("misJuegos")) || [];
+
+    if (juegosComprados.length === 0) {
+        lista.innerHTML = "<p>No has comprado juegos aún 😢</p>";
+        return;
+    }
+
+    juegosComprados.forEach(juego => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+
+        div.innerHTML = `
+            <img src="img/default.jpg">
+            <h3>${juego.nombre}</h3>
+            <p>$${juego.precio}</p>
+            <p>✅ Comprado</p>
+        `;
+
+        lista.appendChild(div);
+    });
 }
